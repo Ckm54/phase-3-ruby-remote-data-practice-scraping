82e59fe3fa3de31b356e3a950f139659c5037671
Collins <mutaic84@gmail.com>
2022-06-18 17:31:50 +0300

    practice web scraping code along

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(URI.open("https://flatironschool.com/"))
+headings = doc.css('.heading-financier').text.strip
+
+courses = doc.css('.heading-20-semibold.color-black.card-title-tl')
+
+binding.pry
+courses.each do |course|
+  puts course.text.strip
+end
+
+# binding.pry
